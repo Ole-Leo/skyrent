c5559078632aca799eda1d2b4fb0ef73a07d9041
Ole_Leo <olesya.leonovich@gmail.com>
2023-02-24 18:54:43 +0300

    feat: add filter clean-up

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,18 @@
 import { ReactNode } from 'react';
+import cn from 'classnames';
+
 import styles from './styles.module.css';
 
 type Props = {
   children: ReactNode;
+  type?: 'normal' | 'outlined';
   onClick?: VoidFunction;
 };
 
-export const Button = ({ children, onClick }: Props) => {
+export const Button = ({ type = 'normal', children, onClick }: Props) => {
+  const btnStyle = cn(styles.button, styles[type]);
   return (
-    <button className={styles.button} onClick={onClick}>
+    <button className={btnStyle} onClick={onClick}>
       {children}
     </button>
   );
