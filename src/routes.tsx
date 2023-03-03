44150bb29fb508c37a9d0bbbaec3880e8f310284
Ole_Leo <olesya.leonovich@gmail.com>
2023-03-03 18:35:53 +0300

    feat: add not found page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { Layout } from './components/Layout/Layout';
 import { MainPage } from './pages/MainPage/MainPage';
 import { PlacePage } from './pages/PlacePage/PlacePage';
@@ -8,10 +8,11 @@ export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route path="/" element={<MainPage />} />
+        <Route index element={<MainPage />} />
         <Route path="/place/:id" element={<PlacePage />} />
         <Route path="/about" element={<AboutPage />} />
       </Route>
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 };
